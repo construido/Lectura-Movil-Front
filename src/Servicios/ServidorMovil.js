253657049426e6598e630f3e00bc7c70b2b9e556
Eligio Eloy Vaca Flores <eligiovaca@hotmail.com>
2022-10-31 09:26:24 -0400

    Implementacion de observacion

--- src/Servicios/ServidorMovil.js
+++ src/Servicios/ServidorMovil.js
@@ -1,6 +1,6 @@
 import BtnAtras from '@/components/BtnAtras.vue'
 import NavBar from '@/components/NavBar.vue'
-import { NombreUsuario, ApellidoUsuario } from './ControlErrores'
+import { SessionExpirada, NombreUsuario, ApellidoUsuario, DataBaseAlias, Plomero } from './ControlErrores'
 import moment from 'moment'
 
 export default {
@@ -13,7 +13,7 @@ export default {
 
     data() {
         return {
-            show: false,
+            show2: false,
             idPlanilla: [],
             arraryPlanillas : [],
             usuario: NombreUsuario() + ' ' + ApellidoUsuario(),
@@ -22,25 +22,40 @@ export default {
             color : '',
             header : '',
             message : '',
+
+            // datos de la number
+            show: false,
+            DataBaseAlias  : DataBaseAlias(),
+            Plomero        : Plomero(),
+            arrayFacturas: [],
+            pagination : {
+                'total' : 0,
+                'current_page' : 0,
+                'per_page' : 0,
+                'last_page' : 0,
+                'from' : 0,
+                'to' : 0,
+            },
+            offset : 2,
         }
     },
 
     methods: {
         WMGet_Lecturas_Pendientes(){
-            this.show = true
+            this.show2 = true
             this.axios.post('admin/WMGet_Lecturas_Pendientes')
             .then( res => {
                 this.arraryPlanillas = res.data
                 console.log(res.data)
-                this.show = false
+                this.show2 = false
             })
             .catch( err => {
                 console.log(err.response)
-                this.show = false
+                this.show2 = false
             })
         },
         WMSincronizacionBDListDemo(){
-            this.show = true
+            this.show2 = true
             this.axios.post('admin/WMSincronizacionBDListDemo', {
                 'GeneracionFactura' : this.idPlanilla
             })
@@ -48,6 +63,7 @@ export default {
                 console.log(res.data)
 
                 if(res.data.status == 1) {
+                    this.listarFacturas()
                     this.color   = 'success'
                     this.header  = res.data.message
                     this.message = res.data.values
@@ -58,20 +74,69 @@ export default {
                     this.message = res.data.values
                 }
 
-                this.show = false
+                this.show2 = false
                 this.$bvModal.show('modal-sincronizacion');
             })
             .catch( err => {
                 console.log(err.response)
-                this.show = false
+                this.show2 = false
             })
         },
         formatoFecha($data){
             return moment().format("DD-MM-YYYY", $data);
-        }
+        },
+
+        // DATOS DE LA NUBE
+        cambiarPagina(page){
+            //Actualiza la página actual
+            this.pagination.current_page = page;
+            this.listarFacturas(page);
+        },
+        listarFacturas(page){
+            this.axios.post('/admin/listarPlanillaDeLecturasPendientes?page='+page+'&DataBaseAlias='+this.DataBaseAlias+'&Plomero='+this.Plomero)
+                .then(res => {
+                    if (res.data.status == 403){
+                        SessionExpirada();
+                    }else{
+                        this.pagination = res.data.values.pagination;
+                        this.arrayFacturas = res.data.values.laGeneracionFactura.data;
+                    }
+                })
+                .catch(e => {
+                    this.arrayFacturas = [];
+                })
+        },
     },
 
     created(){
-        this.WMGet_Lecturas_Pendientes();
+        this.listarFacturas()
+        this.WMGet_Lecturas_Pendientes()
+    },
+
+    computed: {
+        isActived: function(){
+            return this.pagination.current_page;
+        },
+        //     //Calcula los elementos de la paginación
+        pagesNumber: function() {
+            if(!this.pagination.to) {
+                return [];
+            }
+            var from = this.pagination.current_page - this.offset; 
+            if(from < 1) {
+                from = 1;
+            }
+
+            var to = from + (this.offset * 2); 
+            if(to >= this.pagination.last_page){
+                to = this.pagination.last_page;
+            }  
+            var pagesArray = [];
+            while(from <= to) {
+                pagesArray.push(from);
+                from++;
+            }
+            return pagesArray;             
+        }
     },
 }
